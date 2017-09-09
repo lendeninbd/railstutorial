43254ce46a83d7ea07fb8935aac778ead04e8441
bcc <shiblyxp@gmail.com>
2017-09-09 16:22:22 +0200

    Ruby on rails 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  resources :users, :only => [:index, :show]
+
+
   get 'posts/find_posts' => 'posts/find_posts'
 
   resources :posts
